c19f904eafb18d9e4ba8a86d1fc7535d5858d0f7
Heejun Song <hjsong@pdlab.kr>
2025-09-14 16:13:12 +0900

    Update auto-closing neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,14 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    event_handlers = {
-      {
-        event = "file_opened",
-        handler = function(file_path)
-          require("neo-tree.command").execute({ action = "close" })
-        end,
-      },
-    },
     filesystem = {
       filtered_items = {
         visible = true,
